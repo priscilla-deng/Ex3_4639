e1f9b102c849b14fc1f543bd46f02542128c18af
priscilla-deng <122641420+priscilla-deng@users.noreply.github.com>
2023-02-02 23:41:55 +0000

    Feb 2, 2023, 6:41 PM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello worlddd");
+const quote1 = "'Actually, life is beautiful and I do have the time.'"
+const quote2 = "'The other day, lying in bed, I felt my heart beating for the first time in a long while. I realized how little I live in my body, how much in my mind.' -Rodger Kamenetz"
+const quote3 = "'I have buried myself in every place I have ever been.'"
+
+const quoteList = [quote1, quote2, quote3];
+
+let num = Math.floor(Math.random() * 3) + 1;
+
+console.log(num);

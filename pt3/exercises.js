2f876278f10eaea6813e45705b5eed87aca1a398
priscilla-deng <122641420+priscilla-deng@users.noreply.github.com>
2023-02-03 00:17:46 +0000

    Feb 2, 2023, 7:17 PM

--- pt3/exercises.js
+++ pt3/exercises.js
@@ -1,6 +1,5 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
-// → [1, 2, 3, 4, 5, 6]
+
 function joinArrays () {
     const joined = arrays[0].concat(arrays[1], arrays[2]);
     console.log(joined);

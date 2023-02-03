b748e0b2566e8a0fc91b74264fc3677121713580
priscilla-deng <122641420+priscilla-deng@users.noreply.github.com>
2023-02-03 00:07:01 +0000

    Feb 2, 2023, 7:07 PM

--- exercises.js
+++ exercises.js
@@ -1,5 +1,10 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
-const joined = arrays[0].concat(arrays[1], arrays[2]);
-console.log(joined);
+function joinArrays () {
+    const joined = arrays[0].concat(arrays[1], arrays[2]);
+    console.log(joined);
+    return joined;
+}
+
+joinArrays(arrays);
